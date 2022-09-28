91c3ed3e340510353d59967afb88f86490ce3ca3
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2022-09-28 14:03:07 -0600

    solution

--- src/app/password/password.component.ts
+++ src/app/password/password.component.ts
@@ -3,7 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-password',
   templateUrl: './password.component.html',
-  styleUrls: ['./password.component.css']
+  styles: [`
+    .white
+    { color: white,
+      }
+    `]
 })
 export class PasswordComponent implements OnInit {
   display = false;
