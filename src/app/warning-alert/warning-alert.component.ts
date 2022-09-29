a8317985e3d19a9a5739f53fe17cec10a8a40f00
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2022-09-29 11:43:55 -0600

    section 5

--- src/app/warning-alert/warning-alert.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'app-warning-alert',
-  templateUrl: './warning-alert.component.html',
-  // template: `<p>Warning!</p>`,
-  // styles: [`p{background-color: red}`]
-})
-export class WarningAlertComponent{}
