4641d58564409ffe46e7ca156490346e4e207621
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2022-09-29 10:58:43 -0600

    section 3

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,4 @@
+export class Ingredient{
+  // adding an accessor (public) will create the same class variables as written conventionally
+  constructor(public name: string, public amount: number) {}
+}
